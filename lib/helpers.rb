16cb33248b7b71c2e9d040464e0fa7a25b814a1a
Marcin Grzywaczewski <killavus@gmail.com>
2013-06-04 03:17:07 +0000

    Added some logic to main usecase

--- lib/helpers.rb
+++ lib/helpers.rb
@@ -1,7 +1,11 @@
 module Endpoints
   refine Class do
     def endpoint(name)
-      self.class_eval "def #{name.to_sym}(*args); end"
+      self.class_eval "def #{name.to_s}(*args); end"
+    end
+
+    def endpoints(*names)
+      names.each { |name| endpoint(name) }
     end
   end
 end
--- usecases/main.rb
+++ usecases/main.rb
@@ -1,11 +1,15 @@
 require_relative 'session_situations'
 require_relative '../models/session'
 require_relative '../models/session_history'
+require_relative '../lib/helpers'
 
 module Pomodoro
   module Usecase
+    using Endpoints
+
     class Main 
       include Policy::SessionSituation
+      endpoints :user_started_session, :user_interrupted_session
       
       def initialize
         @sessions = SessionHistory.new
@@ -19,13 +23,13 @@ module Pomodoro
         end
       end
 
-      def user_wants_break
-        spawn_new_break
-      end
+      def user_wants_break; spawn_new_break; end
+      def user_skips_break; spawn_new_pomodoro; end
+      def user_interrupts_session; @actual_session = nil; end
 
       def user_clicks_button
         if during_session
-          interrupt_session  
+          user_interrupts_session
         else
           spawn_new_pomodoro
         end
